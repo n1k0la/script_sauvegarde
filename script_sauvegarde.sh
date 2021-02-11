4501b56562b1e6bc03eb8259965092a756e02b40
n1k0la <addewulf@protonmail.ch>
2021-02-11 06:00:27 +0100

    Nouvelle fonction, création d'un log.
    Le script sauvegarde crée un fichier.log lié à l'archive.

--- script_sauvegarde.sh
+++ script_sauvegarde.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+script "$(date '+%Y-%w')_sauvegarde-documents.log"
 tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public
 
+script "$(date '+%Y-%w')_sauvegarde-divertissement.log"
 tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads
