d19e6bfa3b9e86ecda48b89e7057d81596fcce21
n1k0la <addewulf@protonmail.ch>
2021-02-15 19:59:39 +0100

    Update script_sauvegarde.sh
    Nouvelle fonctionnalité: création d'un fichier texte qui enregistre l'archivage de la commande tar.

--- script_sauvegarde.sh
+++ script_sauvegarde.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-script "$(date '+%Y-%w')_sauvegarde-documents.log"
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public
+tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public > "$(date '+%Y-%w')_sauvegarde-documents.log"
+tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads > "$(date '+%Y-%w')_sauvegarde-divertissement.log"
 
-script "$(date '+%Y-%w')_sauvegarde-divertissement.log"
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads
