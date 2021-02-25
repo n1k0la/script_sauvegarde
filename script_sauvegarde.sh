1c7b08e796592c64f5a3c7fd997f1d9cd7ddf166
n1k0la <addewulf@protonmail.ch>
2021-02-25 12:21:13 +0100

    Update script_sauvegarde.sh
    Correction du formatage des années et des semaines.

--- script_sauvegarde.sh
+++ script_sauvegarde.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public > "$(date '+%Y-%w')_sauvegarde-documents.log"
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads > "$(date '+%Y-%w')_sauvegarde-divertissement.log"
+tar -zcvf "$(date '+%G-%V')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public > "$(date '+%G-%V')_sauvegarde-documents.log"
+tar -zcvf "$(date '+%G-%V')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads > "$(date '+%G-%V')_sauvegarde-divertissement.log"
