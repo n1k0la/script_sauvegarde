64a97f66370a196f99ce9d7d29724ae3830067db
n1k0la <addewulf@protonmail.ch>
2021-02-25 12:09:33 +0100

    N1k0la creationn txt.log (#4)
    * Mise à jour insertion automatique de la date. 
    
    Nouvelle fonction: insertion automatique de la date (année+semaine) dans le nom des archives.
    
    * Nouvelle fonction, création d'un log.
    
    Le script sauvegarde crée un fichier.log lié à l'archive.
    
    * Update script_sauvegarde.sh
    
    Nouvelle fonctionnalité: création d'un fichier texte qui enregistre l'archivage de la commande tar.

--- script_sauvegarde.sh
+++ script_sauvegarde.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public
-
-tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertainments Downloads
+tar -zcvf "$(date '+%Y-%w')_sauvegarde-documents.tar.gz" .mozilla Desktop Documents Public > "$(date '+%Y-%w')_sauvegarde-documents.log"
+tar -zcvf "$(date '+%Y-%w')_sauvegarde-divertissement.tar.gz" Entertaiments Downloads > "$(date '+%Y-%w')_sauvegarde-divertissement.log"
